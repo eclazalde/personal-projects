4d783383612bc33895781c74299ce134a2b5b6f8
Eric <eclazalde@hotmail.com>
2014-04-18 01:13:51 -0600

    finished lab5 plots and have gathered data

--- proj5/plot.py
+++ proj5/plot.py
@@ -12,13 +12,13 @@ from pylab import *
 # Class that parses a file and plots several graphs
 class Plotter:
     def __init__(self):
-        times = []
+        '''times = []
         count = 0
         conn = httplib.HTTPConnection("localhost", 3000)
         
-        '''while count < 100:
+        while count < 100:
             startTime = time.time()
-            conn.request("GET",  "/static/files/largefile.txt", None)
+            conn.request("GET",  "/static/img/beehive.png", None)
             response = conn.getresponse()
             
             if response.status == httplib.OK:
@@ -46,10 +46,10 @@ class Plotter:
             self.x.append(xLambda / mu)
             xLambda =xLambda + 1
         
-        f = open('myserver_data', 'w')
+        f = open('lighttpd_data.txt', 'w')
         
         for i in self.y:
-            f.write(str(i) + '\n')
+            f.write(str(i) + ' ' + '\n')
         f.close()
         print average
         print mu'''
@@ -67,10 +67,10 @@ class Plotter:
         """ Create a line graph. """
         clf()
         plot(self.x,self.y)
-        ylim(0,.1)
+        ylim(0,.01)
         xlabel('(Utilization (lambda/mu))')
         ylabel('(Server Response Time (1/mu-lambda))')
-        title('Lab 4 Server Response Time')
+        title('Lighttpd Server Response Time')
         savefig('line.png')
 
     def boxPlot(self):
@@ -79,7 +79,7 @@ class Plotter:
         self.x = []
         self.y = []
         total = 168
-        f = open('tests/my10.txt').readlines()
+        f = open('tests/my1000.txt').readlines()
         for i in f:
            self.y.append(float(i.split(' ')[5]))
            
@@ -90,16 +90,83 @@ class Plotter:
         ylabel('Y Label (units)')
         savefig('boxplot.png')
 
-    def combinedPlot(self):
+    def combinedPlot1(self):
         """ Create a graph that includes a line plot and a boxplot. """
         clf()
-        # plot the line
-        plot(self.x,self.averages)
+        # line stuff
+        lines = []
+        xlines = []
+        lc = 0
+        l = open('myserver_data.txt', 'r').readlines()
+        for line in l:
+            lines.append(float(line))
+            xlines.append(float(lc / float(1579)))
+            #print ('%s, %s') % (float(lc / 1579), line)
+            lc += 1
+        # boxplot stuff
+        self.x = []
+        self.y = []
+        num = 1
+        while num < 11:
+            group = []
+            fil = ('tests/my-load' + str(num) + '0.txt')
+            b = open(fil, 'r').readlines()
+            for line in b:
+                group.append(float(line.split(' ')[5]))
+            self.y.append(group)
+            self.x.append(float(num) / 10)
+            num += 1
         # plot the boxplot
-        boxplot(self.y,positions=self.x,widths=0.5)
-        xlabel('X Label (units)')
-        ylabel('Y Label (units)')
-        savefig('combined.png')
+        #xlim(0, 1.)
+        #ylim(0,.01)
+        boxplot(self.y,positions=self.x,widths=0.05)
+        # plot the line
+        xlim(0,1.1)
+        ylim(0, .1)
+        plot(xlines,lines)
+        xlabel('(Utilization (lambda/mu))')
+        ylabel('(Server Response Time (1/mu-lambda))')
+        title('Lab 4 Server Response Time')
+        savefig('myserver_combined.png')
+
+    def combinedPlot2(self):
+        """ Create a graph that includes a line plot and a boxplot. """
+        clf()
+        # line stuff
+        lines = []
+        xlines = []
+        lc = 0
+        l = open('lighttpd_data.txt', 'r').readlines()
+        for line in l:
+            lines.append(float(line))
+            xlines.append(float(lc / float(1579)))
+            #print ('%s, %s') % (float(lc / 1579), line)
+            lc += 1
+        # boxplot stuff
+        self.x = []
+        self.y = []
+        num = 1
+        while num < 11:
+            group = []
+            fil = ('tests/lt-load' + str(num) + '0.txt')
+            b = open(fil, 'r').readlines()
+            for line in b:
+                group.append(float(line.split(' ')[5]))
+            self.y.append(group)
+            self.x.append(float(num) / 10)
+            num += 1
+        # plot the boxplot
+        #xlim(0, 1.)
+        #ylim(0,.01)
+        boxplot(self.y,positions=self.x,widths=0.05)
+        # plot the line
+        xlim(0,1.1)
+        ylim(0, .1)
+        plot(xlines,lines)
+        xlabel('(Utilization (lambda/mu))')
+        ylabel('(Server Response Time (1/mu-lambda))')
+        title('Lighttpd Server Response Time')
+        savefig('lighttpd_combined.png')
 
     def histogramPlot(self):
         """ Create a histogram. """
@@ -111,6 +178,7 @@ if __name__ == '__main__':
     p = Plotter()
     #p.equationPlot()
     #p.linePlot()
-    p.boxPlot()
-    #p.combinedPlot()
+    #p.boxPlot()
+    p.combinedPlot1()
+    p.combinedPlot2()
     #p.histogramPlot()
